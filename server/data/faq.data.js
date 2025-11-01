42b1eeca3cd68a406aa055e9024540ea50f106e5
clarissaela <cl0001ya@e.ntu.edu.sg>
2025-11-01 14:49:37 +0800

    added FAQ questions

--- server/data/faq.data.js
+++ server/data/faq.data.js
@@ -1,10 +1,18 @@
 const FAQData =[{
-    "qn" : "What is this website for",
+    "qn" : "What is this website for?",
     "ans": "Educating the public on alternatives to throwing products away"
 },
 {
-    "qn" : "Why recycle",
+    "qn" : "Why recycle?",
     "ans": "The planet is dying"
 },
+{
+    "qn" : "What kind of items can be donated/resold/repaired?",
+    "ans": "Check with individual locations on what they accept"
+},
+{
+    "qn" : "What if I have a question not listed here?",
+    "ans": "Please use our chatbot or send us an enquiry"
+},                    
 ]
 export default FAQData
