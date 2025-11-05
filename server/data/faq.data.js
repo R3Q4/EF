834a56c686bfccbef510b2a12cf3e0602ec6615b
clarissaela <cl0001ya@e.ntu.edu.sg>
2025-11-05 12:08:14 +0800

    added more faq

--- server/data/faq.data.js
+++ server/data/faq.data.js
@@ -7,12 +7,20 @@ const FAQData =[{
     "ans": "The planet is dying"
 },
 {
+    "qn:" : "How does recycling help the environment?",
+    "ans": "It saves raw materials and energy, reduces landfill waste and pollution, lowers greenhouse gas emissions and encourages responsible consumption habits."
+},
+{
+    "qn" : "What does “sustainable consumption” mean?",
+    "ans": "It means buying only what you need, choosing durable and ethically produced products, and supporting local and eco-conscious businesses."
+},
+{
     "qn" : "What kind of items can be donated/resold/repaired?",
     "ans": "Check with individual locations on what they accept"
 },
 {
     "qn" : "What if I have a question not listed here?",
     "ans": "Please use our chatbot or send us an enquiry"
-},                    
+},                 
 ]
 export default FAQData
