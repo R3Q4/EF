d3f3c8207bc09e7bcab30635b59ce1e50e6ebecc
R3Q4 <reneeteoruining@gmail.com>
2025-11-07 15:33:49 +0800

    Update faq.data.js

--- server/data/faq.data.js
+++ server/data/faq.data.js
@@ -1,13 +1,13 @@
 const FAQData =[{
     "qn" : "What is this website for?",
-    "ans": "Educating the public on alternatives to throwing products away"
+    "ans": "This website educates the public about alternatives to throwing products away and promotes sustainable practices."
 },
 {
-    "qn" : "Why recycle?",
-    "ans": "The planet is dying"
+    "qn" : "Why should I recycle?",
+    "ans": "Recycling helps everyone play their part in reducing waste, conserving resources, and protecting the environment."
 },
 {
-    "qn:" : "How does recycling help the environment?",
+    "qn" : "How does recycling help the environment?",
     "ans": "It saves raw materials and energy, reduces landfill waste and pollution, lowers greenhouse gas emissions and encourages responsible consumption habits."
 },
 {
